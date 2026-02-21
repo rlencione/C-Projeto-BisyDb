a47f2d7f043cd6e36fdd60b7d573d9f7549d66f0
RLencione <rogar.code@gmalil.com>
2026-02-21 02:20:01 -0300

    atualiz 2002

--- Main.py
+++ Main.py
@@ -66,13 +66,13 @@ def enviar_dados():
 
 # Criando janela principal
 
-img = Image.open("C:\Projetos\BisyDb\image_bisy\logo.png")
+img = Image.open(r'C:\Projetos\BisyDb\image_bisy\logo.png')
 img.save("logo.ico", format="ICO")
 
 
 janela = tk.Tk()
 janela.title("BisyDb")
-janela.iconbitmap("logo.ico") #sizes=[(32,32), (64,64), (128,128)]))
+janela.iconbitmap("logo.ico")
 janela.geometry("1360x720+0+0")
 janela.resizable(False, False)
 janela.config(bg="#435059")
@@ -106,6 +106,20 @@ frame_4 = tk.Frame(janela,
                    height=30,
                    )
 frame_4.grid(column=0, row=3, columnspan=15)
+frame_7 = tk.Frame(janela,
+                   background="#435059",
+                   width=1360,
+                   height=30,
+                   )
+frame_7.grid(column=0, row=7, columnspan=15)
+frame_10 = tk.Frame(janela,
+                   background="#435059",
+                   width=1360,
+                   height=30,
+                   )
+frame_10.grid(column=0, row=10, columnspan=15)
+
+
 # Labels e Entradas
 
 # Combo base
@@ -119,11 +133,8 @@ label_base = tk.Label(
 )
 label_base.grid(column=0, row=4)
 
-
 def mostrar_base():
     print("Selecionado:", base.get())
-
-
 bases = ["GRU", "GIG", "SJC"]
 base = ttk.Combobox(
     janela,
@@ -131,9 +142,8 @@ base = ttk.Combobox(
     font=("Arial", 12, "bold",),
     width=11,
 )
-base.grid(column=0, row=5, padx=10)
-# base.current(0)
 base.bind("<<ComboboxSelected>>", mostrar_base)
+base.grid(column=0, row=5, padx=10)
 
 # bislog
 label_bis_log = tk.Label(
@@ -152,11 +162,9 @@ bis_log = tk.Entry(
     font=("Arial", 12, "bold"),                # fonte e tamanho
     width=11,
 )
-
 bis_log.grid(column=1, row=5, padx=10)
 
 # Combo Client
-
 label_client = tk.Label(
     janela,
     bg="#435059",
@@ -167,11 +175,8 @@ label_client = tk.Label(
 )
 label_client.grid(column=2, row=4)
 
-
 def mostrar_client():
     print("Selecionado:", client.get())
-
-
 clients = ["AVIANCA", "AIR MAROC", "ARAJET", "SKY",]
 client = ttk.Combobox(
     janela,
@@ -179,12 +184,10 @@ client = ttk.Combobox(
     font=("Arial", 12, "bold",),
     width=11,
 )
-client.grid(column=2, row=5, padx=10)
-# client.current(0)
 client.bind("<<ComboboxSelected>>", mostrar_client)
+client.grid(column=2, row=5, padx=10)
 
-
-# Flight NUmber
+# Combo Flight NUmber
 label_flight = tk.Label(
     janela,
     bg="#435059",
@@ -194,16 +197,21 @@ label_flight = tk.Label(
     fg="white",
 )
 label_flight.grid(column=3, row=4)
-flight = tk.Entry(
+
+def mostrar_flight():
+    print("Selecionado:", flight.get())
+flights_avianca = ["AV86", "AV184", "AV249", "AV161",]
+flight = ttk.Combobox(
     janela,
-    bg="white",
-    fg="black",
-    font=("Arial", 12, "bold"),
-    width=11
+    values=flights_avianca,
+    font=("Arial", 12, "bold",),
+    width=11,
 )
+# client.current(0)
+flight.bind("<<ComboboxSelected>>", mostrar_flight())
 flight.grid(column=3, row=5, padx=10)
 
-# Aircraft
+# Combo Aircraft
 label_acft = tk.Label(
     janela,
     bg="#435059",
@@ -213,13 +221,28 @@ label_acft = tk.Label(
     fg="white",
 )
 label_acft.grid(column=4, row=4)
-acft = tk.Entry(
+
+def mostrar_acft():
+    print("Selecionado:", acft.get())
+acfts_avianca = ["A320 NEO","HC-CRU","HC-CJV","HC-CJW","HC-CSF","HC-CTX","HK-5273","HK-5318","HK-5319","HK-5320","HK-5335","HK-5352",
+"HK-5353","HK-5360","HK-5361","HK-5365","HK-5366","HK-5367","HK-5368","HK-5378","HK-5388","HK-5389","HK-5390","HK-5393","HK-5395",
+"HK-5406","HK-5407","HK-5408","HK-5409","HK-5410","HK-5421","HK-5422","HK-5423","HK-5424","HK-5425","N195AV","N206FR","N207FR",
+"N230AC","N253AC",'N281AV',"N284AV","N345AV","N398AV","N401AV","N411AE","N411AV","N416AV","N422AV","N426AV","N430AV","N446AV",
+"N451AV","N454AV","N477AV","N481AV","N519AV","N536AV","N538AV","N557AV","N562AV","N567AV","N599AV","N632AV","N647AV","N664AV",
+"N686TA","N688TA","N689TA","N691AV","N723AV","N724AV","N726AV","N728AV","N740AV","N741AV","N742AV","N743AV","N745AV","N748AV",
+"N750AV","N755AV","N762AV","N763AV","N764AV","N765AV","N766AV","N769AV","N775AV","N776AV","N779AV","N788AV","N818AV","N821AV",
+"N862AV","N901AV","N902AV","N903AV","N904AV","N905AV","N906AV","N908AV","N909AV","N919AV","N920AV","N920CG","N930AV","N931AV",
+"N932AV","N934AG","N937AV","N938AV","N939AV","N940AV","N942AV","N943AV","N944AV","N945AV","N946AV","N948AV","N950AV","N951AV",
+"N954AV","N955AV","N956AV","N957AV","N958AV","N959AV","N960AV","N961AV","N962AV","N963AV","N964AV","N965AV","N966AV","N967AV",
+"N980AV","N992AV","PR-AVC","B787/800"
+]
+acft = ttk.Combobox(
     janela,
-    bg="white",
-    fg="black",
-    font=("Arial", 12, "bold"),
-    width=11
+    values=acfts_avianca,
+    font=("Arial", 12, "bold",),
+    width=11,
 )
+acft.bind("<<ComboboxSelected>>", mostrar_acft)
 acft.grid(column=4, row=5, padx=10)
 
 # Logbook
@@ -242,9 +265,7 @@ logbook = tk.Entry(
 logbook.grid(column=5, row=5, padx=10)
 
 # date
-
 # DateEntry(locale='pt_PT', date_pattern='MM/dd/yyyy')
-
 label_log_date = tk.Label(
     janela,
     bg="#435059",
@@ -264,61 +285,73 @@ log_date = DateEntry(
 )
 log_date.grid(column=6, row=5, padx=10)
 
-# oil_lh
+# Combo oil_lh
 label_oil_lh = tk.Label(
     janela,
     text="LH",
     bg="#435059",
     font=("Arial", 12, "bold"),
-    width=4,
+    width=3,
     fg="white",
 )
 label_oil_lh.grid(column=7, row=4)
-oil_lh = tk.Entry(
+
+def mostrar_oil_lh():
+    print("Selecionado:", oil_lh.get())
+oil_lhs = (1, 2, 3, 4, 5, 6)
+oil_lh = ttk.Combobox(
     janela,
-    bg="white",                  # cor de fundo
-    fg="black",                         # cor do texto
-    font=("Arial", 12, "bold"),                # fonte e tamanho
-    width=5
+    values=oil_lhs,
+    font=("Arial", 12, "bold",),
+    width=3,
 )
+client.bind("<<ComboboxSelected>>", mostrar_oil_lh)
 oil_lh.grid(column=7, row=5, padx=10)
 
-# oil_rh
+# Combo oil_rh
 label_oil_rh = tk.Label(
     janela,
     text="RH",
     bg="#435059",
     font=("Arial", 12, "bold"),
-    width=4,
+    width=3,
     fg="white",
 )
 label_oil_rh.grid(column=8, row=4)
-oil_rh = tk.Entry(
+
+def mostrar_oil_rh():
+    print("Selecionado:", oil_rh.get())
+oil_rhs = (1, 2, 3, 4, 5, 6)
+oil_rh = ttk.Combobox(
     janela,
-    bg="white",                  # cor de fundo
-    fg="black",                         # cor do texto
-    font=("Arial", 12, "bold"),                # fonte e tamanho
-    width=5
+    values=oil_rhs,
+    font=("Arial", 12, "bold",),
+    width=3,
 )
+oil_rh.bind("<<ComboboxSelected>>", mostrar_oil_rh)
 oil_rh.grid(column=8, row=5, padx=10)
 
-# oil_apu
+# Comob oil_apu
 label_oil_apu = tk.Label(
     janela,
     text="APU",
     bg="#435059",
     font=("Arial", 12, "bold"),
-    width=4,
+    width=3,
     fg="white",
 )
 label_oil_apu.grid(column=9, row=4)
-oil_apu = tk.Entry(
+
+def mostrar_oil_apu():
+    print("Selecionado:", oil_apu.get())
+oil_apus = (1, 2, 3, 4, 5, 6)
+oil_apu = ttk.Combobox(
     janela,
-    bg="white",                  # cor de fundo
-    fg="black",                         # cor do texto
-    font=("Arial", 12, "bold"),                # fonte e tamanho
-    width=5
+    values=oil_apus,
+    font=("Arial", 12, "bold",),
+    width=3,
 )
+oil_apu.bind("<<ComboboxSelected>>", mostrar_oil_apu)
 oil_apu.grid(column=9, row=5, padx=10)
 
 # technician
@@ -332,22 +365,126 @@ label_log_AMT = tk.Label(
 )
 label_log_AMT.grid(column=10, row=4)
 
-
 def mostrar_log_AMT():
     print("Selecionado:", log_AMT.get())
-
-
 log_AMTs = ["COELHO_014", "LENCIONE_092", "MARCELO_033",
-            "MELO_076", "MAURICIO", "RICARDO_085", "SAMUEL"]
+            "MELO_076", "MAURICIO_006", "RICARDO_085", "SAMUEL"]
 log_AMT = ttk.Combobox(
     janela,
     values=log_AMTs,
     font=("Arial", 10, "bold"),
     width=15,
 )
-log_AMT.grid(column=10, row=5, padx=10)
-# base.current(0)
 log_AMT.bind("<<ComboboxSelected>>", mostrar_log_AMT())
+log_AMT.grid(column=10, row=5, padx=10)
+
+# item mel
+label_item_mel = tk.Label(
+    janela,
+    text="DEFERRAL",
+    bg="#435059",
+    font=("Arial", 12, "bold"),
+    width=9,
+    fg="white",
+)
+label_item_mel.grid(column=1, row=11)
+
+def mostrar_item_mel():
+    print("Selecionado:", item_mel.get())
+item_mels = ["MEL", "NEF", "CDL", "EO"]
+item_mel = ttk.Combobox(
+    janela,
+    values=item_mels,
+    font=("Arial", 10, "bold"),
+    width=15,
+)
+item_mel.bind("<<ComboboxSelected>>", mostrar_item_mel())
+item_mel.grid(column=1, row=12, padx=10)
+
+# ata mel
+label_ata_mel = tk.Label(
+    janela,
+    text="ATA CHPT",
+    bg="#435059",
+    font=("Arial", 12, "bold"),
+    width=9,
+    fg="white",
+)
+label_ata_mel.grid(column=2, row=11)
+
+def mostrar_ata_mel():
+    print("Selecionado:", ata_mel.get())
+ata_mels = ["21-00-00", "22-00-00","23-00-00","24-00-00","25-00-00"]
+ata_mel = ttk.Combobox(
+    janela,
+    values=ata_mels,
+    font=("Arial", 10, "bold"),
+    width=15,
+)
+ata_mel.bind("<<ComboboxSelected>>", mostrar_ata_mel())
+ata_mel.grid(column=2, row=12, padx=10)
+
+# cat mel
+label_cat_mel = tk.Label(
+    janela,
+    text="MEL CAT",
+    bg="#435059",
+    font=("Arial", 12, "bold"),
+    width=9,
+    fg="white",
+)
+label_cat_mel.grid(column=3, row=11)
+
+def mostrar_cat_mel():
+    print("Selecionado:", cat_mel.get())
+cat_mels = ["CAT_A","CAT_B","CAT_C","CAT_D"]
+cat_mel = ttk.Combobox(
+    janela,
+    values=cat_mels,
+    font=("Arial", 10, "bold"),
+    width=15,
+)
+cat_mel.bind("<<ComboboxSelected>>", mostrar_cat_mel())
+cat_mel.grid(column=3, row=12, padx=10)
+
+# Checkbuttons report
+no_report = tk.IntVar()
+yes_report = tk.IntVar()
+
+check_repot = tk.Checkbutton(
+    janela,
+    text="NO REPORT",
+    font = ("Arial", 12, "bold"),
+    fg="white",
+    bg="#435059",      
+)
+check_repot.grid(column=1, row=8)
+
+# Checkbuttons pilot rep
+crew_report = tk.IntVar()
+crew_report = tk.IntVar()
+
+pilot_repot = tk.Checkbutton(
+    janela,
+    text="PILOT REP",
+    font = ("Arial", 12, "bold"),
+    fg="white",
+    bg="#435059",      
+)
+pilot_repot.grid(column=2, row=8)
+
+# Checkbuttons techn rep
+maint_report = tk.IntVar()
+maint_report = tk.IntVar()
+
+tech_report = tk.Checkbutton(
+    janela,
+    text="TECH REP",
+    font = ("Arial", 12, "bold"),
+    fg="white",
+    bg="#435059",      
+)
+tech_report.grid(column=3, row=8)
 
 # Report
 label_report = tk.Label(
@@ -358,15 +495,16 @@ label_report = tk.Label(
     width=10,
     fg="white",
 )
-label_report.place(x=15, y=195)
+label_report.grid(column= 0, row=8)
 report = tk.Text(
     janela,
     bg="white",
     fg="black",
     font=("Arial", 12, "bold"),
-    height=5, width=70,
+    height=5, width=62,
 )
-report.place(x=10, y=220)
+report.grid(column=0, row=9, columnspan=4)
+
 # Action
 label_action = tk.Label(
     janela,
@@ -376,16 +514,15 @@ label_action = tk.Label(
     width=10,
     fg="white",
 )
-label_action.place(x=15, y=355)
+label_action.grid(column=0, row=12,)
 action = tk.Text(
     janela,
     bg="white",                  # cor de fundo
     fg="black",                         # cor do texto
     font=("Arial", 12, "bold"),                # fonte e tamanho
-    height=5, width=70,
+    height=5, width=62,
 )
-action.place(x=10, y=380,)
-
+action.grid(column=0, row=13, columnspan=4)
 # Image
 # label_image = tk.Label(
 # janela,
